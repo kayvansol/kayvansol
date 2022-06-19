6154239f4e398f6cb2916f4dcfdfde9755fcb188
Kayvan Soleimani <53554624+kayvansol@users.noreply.github.com>
2022-06-19 22:06:45 +0430

    Update JDBCUtil.java

--- JDBCUtil.java
+++ JDBCUtil.java
@@ -3,19 +3,24 @@ import java.sql.DriverManager;
  
 /**
  * This is a utility class for JDBC connection.
- * @author Kayvan Soleimani
+ * @author : Kayvan Soleimani
  */
 public class JDBCUtil {
+	
 	//JDBC and database properties.
 	private static final String DB_DRIVER = 
 		           "oracle.jdbc.driver.OracleDriver";
+	
 	private static final String DB_URL = 
 		        "jdbc:oracle:thin:@localhost:1521:XE";
+	
 	private static final String DB_USERNAME = "system";
 	private static final String DB_PASSWORD = "oracle";
  
 	public static Connection getConnection(){
+		
 		Connection conn = null;
+		
 		try{
 			//Register the JDBC driver
 			Class.forName(DB_DRIVER);
