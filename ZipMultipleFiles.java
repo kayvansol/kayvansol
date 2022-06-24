74fec43a757ef519b680b223b1a2e1959c1c2ee4
Kayvan Soleimani <53554624+kayvansol@users.noreply.github.com>
2022-06-24 13:11:37 +0430

    Update ZipMultipleFiles.java

--- ZipMultipleFiles.java
+++ ZipMultipleFiles.java
@@ -1,8 +1,11 @@
 public class ZipMultipleFiles {
+    
     public static void main(String[] args) throws IOException {
+        
         List<String> srcFiles = Arrays.asList("test1.txt", "test2.txt");
         FileOutputStream fos = new FileOutputStream("multiCompressed.zip");
         ZipOutputStream zipOut = new ZipOutputStream(fos);
+        
         for (String srcFile : srcFiles) {
             File fileToZip = new File(srcFile);
             FileInputStream fis = new FileInputStream(fileToZip);
@@ -11,11 +14,14 @@ public class ZipMultipleFiles {
 
             byte[] bytes = new byte[1024];
             int length;
+            
             while((length = fis.read(bytes)) >= 0) {
                 zipOut.write(bytes, 0, length);
             }
+            
             fis.close();
         }
+        
         zipOut.close();
         fos.close();
     }
