d52358fc9db7655a8d9a89a30d9b35e5beb209fc
Kayvan Soleimani <53554624+kayvansol@users.noreply.github.com>
2022-06-27 22:56:42 +0430

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,6 +4,7 @@ class BubbleSort {
     void bubbleSort(int arr[])
     {
         int n = arr.length;
+        
         for (int i = 0; i < n - 1; i++)
             for (int j = 0; j < n - i - 1; j++)
                 if (arr[j] > arr[j + 1]) {
@@ -18,8 +19,10 @@ class BubbleSort {
     void printArray(int arr[])
     {
         int n = arr.length;
+        
         for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
+        
         System.out.println();
     }
  
